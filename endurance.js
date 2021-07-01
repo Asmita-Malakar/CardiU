f185c1f83e9ee165dc894e06edcb54f9dabcb3f0
Asmita-Malakar <amalakar@students.wcpss.net>
2021-07-01 14:27:27 -0400

    first commit

---
+++ endurance.js
@@ -0,0 +1,184 @@
+$(document).ready(function() {
+    $('#buttonvideo').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
+  
+  function changeImage() {
+  
+      if (document.getElementById("imgclick").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+      {
+          document.getElementById("imgclick").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+      }
+      else if (document.getElementById("imgclick").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+      {
+          document.getElementById("imgclick").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+      }
+  }
+  
+  function changeImage() {
+  
+      if (document.getElementById("imgclick1").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+      {
+          document.getElementById("imgclick1").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+      }
+      else if (document.getElementById("imgclick1").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+      {
+          document.getElementById("imgclick1").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+      }
+  }
+      
+      // if (document.getElementById("imgclick1").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick1").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick1").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick1").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+  
+      
+   //	if (document.getElementById("imgclick2").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick2").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg")
+   //   }
+   //   else if (document.getElementById("imgclick2").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick2").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+   //   if (document.getElementById("imgclick3").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick3").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick3").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick3").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }	 
+   //   if (document.getElementById("imgclick4").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick4").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick4").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick4").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+      // if (document.getElementById("imgclick5").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick5").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick5").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick5").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+  
+      
+   //	if (document.getElementById("imgclick6").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick6").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg")
+   //   }
+   //   else if (document.getElementById("imgclick6").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick6").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+   //   if (document.getElementById("imgclick7").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick7").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick7").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick7").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+   //   if (document.getElementById("imgclick8").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick8").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick8").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick8").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+      // if (document.getElementById("imgclick9").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick9").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick9").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick9").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+  
+      
+   //	if (document.getElementById("imgclick10").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick10").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg")
+   //   }
+   //   else if (document.getElementById("imgclick10").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick10").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+   //   if (document.getElementById("imgclick11").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick11").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick11").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick11").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }	 
+   //   if (document.getElementById("imgclick12").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick12").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick12").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick12").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+      // if (document.getElementById("imgclick13").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick13").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick13").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick13").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+  
+      
+   //	if (document.getElementById("imgclick14").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick14").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg")
+   //   }
+   //   else if (document.getElementById("imgclick14").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick14").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+   //   if (document.getElementById("imgclick15").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick15").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick15").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick15").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+   //   if (document.getElementById("imgclick16").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick16").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg")
+   //   }
+   //   else if (document.getElementById("imgclick16").src = "https:goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick16").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+      
+   //   if (document.getElementById("imgclick17").src == "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg") 
+   //   {
+   //       document.getElementById("imgclick17").src = "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg";
+   //   }
+   //   else if (document.getElementById("imgclick17").src == "https://goodshovel75.qoom.space/cardiU/Images/x-square.svg") 
+   //   {
+   //       document.getElementById("imgclick17").src = "https://goodshovel75.qoom.space/cardiU/Images/check-circle.svg";
+   //   }
+  //}
